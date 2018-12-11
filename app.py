39a1c349573d0cdfdb21b52bf21b2405561bf5e3
Syahiirah <182012W@mymail.nyp.edu.sg>
2018-12-11 12:47:26 +0800

    codes

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
+from flask import request
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('NavBar.html')
 
-@app.route("/TrackProg")
+'''@app.route("/TrackProg")
 def TrackProg():
-    return render_template('TrackProg.html')
+    return render_template('TrackProg.html')'''
 
 @app.route('/TrackProg', methods=('GET', 'POST'))
-def register():
+def add():
+    TraTab = []
+    if request.method == 'POST':
+        exer = request.form['exer']
+        hours = request.form['hours']
+        mins = request.form['mins']
+        error = None
+
+        TraTab[datetime.date] = exer, hours, mins
     return render_template('TrackProg.html')
+
 
 
 if __name__ == '__main__':
