3db3038373302cdfd8e0b4f2c8962a6991f3fe34
182979J <44602043+182979J@users.noreply.github.com>
2019-01-28 21:45:50 +0800

    Add files via upload

--- CreateBlog.py
+++ CreateBlog.py
@@ -1,19 +1,102 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, url_for, redirect, session
+from persistence4 import *
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
+
+UPLOAD_FOLDER = '\\static\\images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/recipes")
+def recipe():
+    items = get_datas()
+    return render_template('RecipeAll.html', posts=items)
 
 
 @app.route("/", methods=("GET", "POST"))
-def testing():
+def create():
+    session['user_name'] = 'pop'
+    username = session['user_name']
     if request.method == "POST":
-        dish_name = request.form['DishName']
-        ingredient = request.form['Ingredients']
-        nutrition = request.form['Nutrition']
-        instructions = request.form['Instructions']
-        return render_template('Recipe.html', dn=dish_name, ind=ingredient, nu=nutrition, ins=instructions)
+        print('000')
+        dishName = request.form['dishName']
+        ingredients = request.form['ingredients']
+        nutrition = request.form['nutrition']
+        instructions = request.form['instructions']
+
+        if 'file' not in request.files:
+            flash('Invalid file')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            flash('file {} saved'.format(file.filename))
+
+            file.save(os.path.join(os.path.abspath('static'), 'images', filename))
+
+            storeData(username, dishName, ingredients, nutrition, instructions, file.filename)
+            return redirect(url_for('recipe', filename=file.filename))
+
+        return render_template("CreateBlog.html", dn=dishName, ind=ingredients, nu=nutrition, ins=instructions,
+                               file=file)
 
     return render_template('CreateBlog.html')
 
 
+@app.route('/<string:id>/update', methods=('GET', 'POST'))
+def update(id):
+    post = get_data(id)
+    print("test-231")
+    username = session['user_name']
+    if request.method == 'POST':
+        print("123321")
+        dishName = request.form['dishName']
+        ingredients = request.form['ingredients']
+        nutrition = request.form['nutrition']
+        instructions = request.form['instructions']
+
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            flash('file {} saved'.format(file.filename))
+
+            file.save(os.path.join(os.path.abspath('static'), 'images', filename))
+
+            updateData(id, username, dishName, ingredients, nutrition, instructions, file.filename)
+            return redirect(url_for('recipe', filename=file.filename))
+
+    return render_template('updateblog.html', post=post)
+
+
+@app.route('/<string:id>/delete', methods=('GET', 'POST'))
+def delete(id):
+    delete_data(id)
+    post = get_data(id)
+    return redirect(url_for('recipe'))
+
+@app.route('/<string:id>/view', methods=('GET', 'POST'))
+def indiRecipe(id):
+    post = get_data(id)
+    return render_template('Recipe.html', post=post)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
+
--- persistence4.py
+++ persistence4.py
@@ -12,17 +12,23 @@ class blog:
         self.instructions = ''
         self.file = ""
         self.created = ""
-        
+        self.username = ''
+        # self.date = ""
+
+
 datas = shelve.open("data")
+
 
 def clear_data():
     klist = list(datas.keys())
     for key in klist:
-        del batas[key]
+        del datas[key]
 
 #noted,LiQi:come back later add file
 #def storeData(dishName, ingredients, nutrition, instructions, file):
-def storeData(dishName, ingredients, nutrition, instructions, file):
+
+
+def storeData(username, dishName, ingredients, nutrition, instructions, file):
     id = str(uuid.uuid4())
     data = blog(id)
     data.dishName = dishName
@@ -30,8 +36,53 @@ def storeData(dishName, ingredients, nutrition, instructions, file):
     data.nutrition = nutrition
     data.instructions = instructions
     data.file = file
+    data.username = username
+    # data.created = str(date.today())
     data.created = str(date.today())
     datas[id] = data
+    print(id)
+    print("endhere")
+def updateData(id,username, dishName, ingredients, nutrition, instructions, file):
+    print(id)
+    data= datas[id]
+
+
+    data.dishName = dishName
+    data.ingredients = ingredients
+    data.nutrition = nutrition
+    data.instructions = instructions
+    data.file = file
+    data.username = username
+    data.created = str(date.today())
+    datas[id] = data
+
+def get_datas():
+    klist = list(datas.keys())
+    x = []
+    for i in klist:
+        x.insert(0, datas[i])
+
+    return x
+
+
+def update_data(data):
+    print(datas)
+    print(data.id)
+    del datas[data.id]
+    datas[data.id] = data
+    print(datas)
+    print("test2")
+
+
+def delete_data(id):
+    if id in datas:
+        del datas[id]
+
+
+def get_data(id):
+    if id in datas:
+        return datas[id]
+
 
 def init_datas():
     clear_data()
@@ -39,40 +90,3 @@ def init_datas():
         storeData('user'+str(i), 'dishName'+str(i), 'ingredients'+str(i), 'nutrition'+str(i), 'instructions'+str(i), 'file')
 
 
-def get_datas():
-    klist = list(datas.keys())
-    x = []
-    for i in klist:
-        x.append(datas[i])
-
-    return x
-
-# def get_latestrecipe():
-#     klist = list(datas.keys())
-#     x = []
-#     if klist:
-#         print('what is here???'+klist[-1])
-#         x.append(klist[-1])
-#
-#     return x
-
-# def update_data(data):
-#     datas[data.id] = data
-#
-# def delete_data(id):
-#     if id in datas:
-#         del datas[id]
-# 
-# def get_data(id):
-#     if id in datas:
-#         return datas[id]
-# 
-# def add_user(user):
-#     users[user.get_id()] = user
-# 
-# def init_db():
-#     clear_user()
-#     clear_data()
-#     for i in range(5):
-#         create_user('user'+str(i), 'pass'+str(i))
-#         storeData('user'+str(i), 'title'+str(i), 'body'+str(i))
