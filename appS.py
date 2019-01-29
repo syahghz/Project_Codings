0bf85a977e7cfaad79cdb2e2fcf2e34a0a6b291b
Syahiirah182012W <182012W@mymail.nyp.edu.sg>
2019-01-29 12:42:46 +0800

    Add files via upload

--- appS.py
+++ appS.py
@@ -14,8 +14,6 @@ tester = 0
 def index():
     return render_template('NavBar.html')
 
-
-
 @appS.route('/TrackProg', methods=('GET', 'POST'))
 def add():
     if request.method == 'POST':
@@ -37,25 +35,9 @@ def add():
                 prevpoints = point_File.read()
                 num = float(prevpoints)
                 point_File.close()
-                points = float(num)
-                if points >= 50:
-                    ncode = gen_num(date, currentUsername)
-                    print(ncode, "passed through PS")
+                points = int(num)
+                return render_template('TrackProg.html', tester=tester, points=points)
 
-                    if ncode[0] == currentUsername:
-                            print(ncode[0], 'usernow')
-                            Rcode = ncode[-2]
-                            print(Rcode, 'the code')
-
-                    point_File = open('point_File' + currentUsername + '.txt', 'w')
-                    aftpoints = points - 50
-                    leftpoints = point_File.write("{}".format(aftpoints))
-                    print(leftpoints)
-                    point_File.close()
-                    return render_template('TrackProg.html', tester=tester, points=points, Rcode=Rcode)
-                else:
-                    print('tkde')
-                    return render_template('TrackProg.html', tester=tester, points= 0)
             else:
                 return render_template('TrackProg.html', tester=tester, points=0)
 
@@ -68,7 +50,7 @@ def add():
             prevpoints = point_File.read()
             num = float(prevpoints)
             point_File.close()
-            points = float(num)
+            points = int(num)
             print(points, 'in else')
             return render_template('TrackProg.html', tester=tester, points=points)
         else:
@@ -76,9 +58,125 @@ def add():
             print('tkde file utk bace')
             return render_template('TrackProg.html', tester=tester, points=points)
 
-
     return render_template('TrackProg.html', tester=tester)
 
+@appS.route('/voucher')
+def voucher():
+    vlist = []
+    currentUsername = 'xuxi'
+    id = str(uuid.uuid4())
+    dt = datetime.datetime.now()
+    date = dt.strftime("%d") + " " + dt.strftime("%B") + " " + dt.strftime("%Y")
+
+
+    if path.exists('point_File' + currentUsername + '.txt'):
+        print('bacefile')
+        point_File = open('point_File' + currentUsername + '.txt', 'r')
+        prevpoints = point_File.read()
+        num = float(prevpoints)
+        point_File.close()
+        points = float(num)
+        if points >= 50:
+                voucher_code = gen_num(id, date, currentUsername)
+                print('moving on')
+                print(voucher_code)
+                k = list(voucher_code.keys())
+                print(k)
+                point_File = open('point_File' + currentUsername + '.txt', 'w')
+                aftpoints = points - 50
+                leftpoints = point_File.write("{}".format(aftpoints))
+                print(leftpoints)
+                point_File.close()
+                print(aftpoints)
+
+                return render_template('voucher.html', vlist=voucher_code, k=k)
+
+        return render_template('voucher.html')
+
+
+    return render_template('voucher.html')
+
+@appS.route('/del')
+def del_list():
+    val = '1'
+    k = list(voucher_code.keys())
+    for i in k:
+        print(i)
+        voucher_code.pop(i)
+        print('wee')
+        return render_template('payment.html')
+    # print(a)
+    # del voucher_code[i]
+
+    redirect(url_for('payment'))
+
+
+
+
+#
+# @appS.route('/TrackProg', methods=('GET', 'POST'))
+# def add():
+#     if request.method == 'POST':
+#             exer = request.form['exer']
+#             hours = request.form['hours']
+#             mins = request.form['mins']
+#             id = str(uuid.uuid4())
+#             dt = datetime.datetime.now()
+#             date = dt.strftime("%d") + " " + dt.strftime("%B") + " " + dt.strftime("%Y")
+#             error = None
+#             currentUsername = 'xuxi' #'vera'
+#             storeBook(id, currentUsername, date, exer, hours, mins)
+#
+#             tester = displaybook(currentUsername)
+#             user_reward = calc_reward(exer, hours, mins, currentUsername)
+#             if path.exists('point_File' + currentUsername + '.txt'):
+#                 print('bacefile')
+#                 point_File = open('point_File' + currentUsername + '.txt', 'r')
+#                 prevpoints = point_File.read()
+#                 num = float(prevpoints)
+#                 point_File.close()
+#                 points = int(num)
+#                 # if points >= 50:
+#                 #     ncode = gen_num(date, currentUsername)
+#                 #     print(ncode, "passed through PS")
+#                 #
+#                 #     if ncode[0] == currentUsername:
+#                 #             print(ncode[0], 'usernow')
+#                 #             Rcode = ncode[-2]
+#                 #             print(Rcode, 'the code')
+#
+#                 #     point_File = open('point_File' + currentUsername + '.txt', 'w')
+#                 #     aftpoints = points - 50
+#                 #     leftpoints = point_File.write("{}".format(aftpoints))
+#                 #     print(leftpoints)
+#                 #     point_File.close()
+#                 #     return render_template('TrackProg.html', tester=tester, points=points)
+#                 # else:
+#                 #     print('tkde')
+#                 #     return render_template('TrackProg.html', tester=tester, points= 0)
+#                 return render_template('TrackProg.html', tester=tester, points=points)
+#             else:
+#                 return render_template('TrackProg.html', tester=tester, points=0)
+#
+#
+#     else:
+#         currentUsername = 'xuxi'
+#         tester = displaybook(currentUsername)
+#         if path.exists('point_File' + currentUsername + '.txt'):
+#             point_File = open('point_File' + currentUsername + '.txt', 'r')
+#             prevpoints = point_File.read()
+#             num = float(prevpoints)
+#             point_File.close()
+#             points = float(num)
+#             print(points, 'in else')
+#             return render_template('TrackProg.html', tester=tester, points=points)
+#         else:
+#             points = 0
+#             print('tkde file utk bace')
+#             return render_template('TrackProg.html', tester=tester, points=points)
+#
+#
+#     return render_template('TrackProg.html', tester=tester)
 
 
 if __name__ == '__main__':
--- persistenceS.py
+++ persistenceS.py
@@ -221,3 +221,224 @@ def gen_num(id,date, username):
             break
         else:
             break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import shelve
+# # import matplotlib.pyplot as plt
+# import random
+# # import matplotlib
+# import os.path
+# from os import path
+# from datetime import datetime
+# from datetime import timedelta
+# # import calendar
+#
+#
+# book = shelve.open("book")
+# x = []
+# voucher_list =[]
+#
+#
+# # def is_empty(list):
+# #     if list:
+# #         print('Structure is not empty.')
+# #         return False
+# #     else:
+# #         print('Structure is empty.')
+# #         return True
+# class AllBooks:
+#     def __init__(self):
+#         self.exercise = ''
+#         self.hour = 0
+#         self.mins = 0
+#         self.date = ""
+#         self.username = ""
+#         print(self.hour)
+#         print('the class')
+#
+#
+#
+#
+# def storeBook(id, username, date, exercise, hour, mins):
+#     bok = AllBooks()
+#     bok.username = username
+#     bok.date = date
+#     bok.exercise = exercise
+#     bok.hour = hour
+#     bok.mins = mins
+#
+#     exist = False
+#
+#     book[id] = bok
+#     print(book[id])
+#     print('masuk storebook')
+#     return bok.exercise
+#
+#
+# def displaybook(currentUserLogin):
+#     #'Renjun': #'vera'
+#     # currentUserLogin = 'Renjun'
+#     klist = list(book.keys())
+#     print(klist)
+#     x.clear()
+#
+#     #if currentUserLogin in klist:
+#     for i in klist:
+#         # print('checking...'+book[i].username)
+#         if book[i].username == currentUserLogin:
+#             print('displaybook', currentUserLogin)
+#             print('showdbook', book[i].username)
+#             # print(x) #check if empty or not
+#             x.insert(0, book[i])
+#     print('masuk display')
+#     return x
+#
+# def calc_reward(exercise, hour, mins, currentUserLogin):
+#     print(currentUserLogin)
+#     print('going in')
+#     reward = AllBooks
+#     # ex = AllBooks(exercise)
+#     reward.exercise = exercise #betul
+#     print(reward.exercise)
+#     reward.hour = float(hour) * 60 #BETUL
+#     print(reward.hour)
+#     reward.mins = float(mins) #BETUL
+#     print(reward.mins)
+#     ttime = float(reward.hour + reward.mins) #BETUL
+#     print(ttime)
+#     print('calculating now')
+#     # hourtoday =
+#     # if hourtoday >= 720:
+#     if reward.exercise == "Vigorous":
+#         val = 5
+#         print(val)
+#         m = float(int(val) * int(ttime))
+#         print(m, "V")
+#         if m > 100:
+#             s = m // 100
+#         elif m >= 10:
+#             s = m // 10
+#         elif m < 10:
+#             s = m
+#
+#     elif reward.exercise == "Moderate":
+#         val = 3
+#         m = float(val * ttime)
+#         print(m, "M")
+#         if m > 100:
+#             s = m // 100
+#             print('divide 100', s)
+#         elif m >= 10:
+#             s = m // 10
+#             print('divide 10', s)
+#         elif m < 10:
+#             s = m
+#             print('tk divide', s)
+#
+#     elif reward.exercise == "Light":
+#         val = 1
+#         m = float(val * ttime)
+#         print(m, "L")
+#         if m > 100:
+#             s = m // 100
+#         elif m >= 10:
+#             s = m // 10
+#         elif m < 10:
+#             s = m
+#     print(s, 'before')
+#     # s += s
+#     # print(s, "ape ni")
+#
+#     print('loop!')
+#     # pstore = list(book.keys())
+#     # for i in pstore:
+#     #     print(i)
+#     #     print(book[i].username, 'the book i')
+#     #     for u in book[i].username:
+#     #         if book[i].username == currentUserLogin:
+#     keys = list(book.keys())
+#     for ind in keys:
+#         print(ind)
+#         # print(book[i].username)
+#         # for u in book[ind].username:
+#         #     print('masuk u')
+#         #     print(u)
+#         #     print(currentUserLogin)
+#         if book[ind].username == currentUserLogin:
+#                 print(book[ind].username)
+#                 print(currentUserLogin)
+#                 if path.exists('point_File' + book[ind].username + '.txt'):
+#                     print('file ade')
+#                     print('alright to reading in file!!')
+#                     point_File = open('point_File' + book[ind].username + '.txt', 'r')
+#                     prevpoints = point_File.read()
+#                     num = float(prevpoints)
+#                     point_File.close()
+#                     print(prevpoints, 'dh baca')
+#
+#                     point_File = open('point_File' + book[ind].username + '.txt', 'w')
+#                     newpoints = (num + int(s))
+#                     print(newpoints)
+#                     npoints = point_File.write( "{}\n".format(newpoints))
+#                     point_File.close()
+#                     print(npoints, 'dh tukar')
+#
+#                     break
+#
+#                 else:
+#                     print('file tkde')
+#                     print(s)
+#                     point_File = open('point_File' + book[ind].username + '.txt', 'w')
+#                     firstpoints = point_File.write( "{}".format(s))
+#                     print(firstpoints)
+#                     point_File.close()
+#
+#                     print('jadi tk')
+#                     break
+#         else:
+#             print('masuk else')
+#             continue
+#
+#     # continue
+#     print('out')
+#
+#
+# def gen_num(date, currentUsername):
+#     currentUserList = []
+#     #generate the code number
+#     num = random.randint(1000,10000)
+#     print(num)
+#     Dcode = "WW" + str(num)
+#     print(Dcode)
+#
+#     #take the expiry date
+#     datexp = (datetime.now() + timedelta(days=5)).strftime('%Y-%m-%d')
+#     print(datexp)
+#
+#     currentUserList.append(currentUsername)
+#     currentUserList.append(date)
+#     currentUserList.append(Dcode)
+#     currentUserList.append(datexp)
+#     print(currentUserList)
+#     return currentUserList
